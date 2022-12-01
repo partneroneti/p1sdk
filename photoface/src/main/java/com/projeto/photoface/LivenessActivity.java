04ae6aec5628955fa1e8a30a454884c283cee018
Aller Allegro <aller.allegro@partnergroup.com.br>
2022-12-01 11:54:07 -0300

    ajuste chamada live
    getSupportActionBar().hide();

--- photoface/src/main/java/com/projeto/photoface/LivenessActivity.java
+++ photoface/src/main/java/com/projeto/photoface/LivenessActivity.java
@@ -19,7 +19,6 @@ public class LivenessActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         Context context = this;
         ftsp = new FaceTecFaceScanProcessor() {
             @Override
